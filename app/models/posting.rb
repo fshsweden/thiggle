86f16013020671d158c1a6db4d8fb9de659c8556
Matt Rickard <ubuntu@ip-10-212-110-228.ec2.internal>
2013-06-06 13:58:29 +0000

    s

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,12 +1,9 @@
 class Posting < ActiveRecord::Base
   attr_accessible :category, :description, :location, :postdate, :postid, :price, :title, :userid
 
-  validates_prescence_of :category, :description, :location, :postdate, :postid, :price, :title, :userid
-  validates_uniqueness_of :postid, :allow_blank => false, :userid
-
-  searchable :autoindex => true, auto_remove true do
-    text :title, :boost =>
-    text :description
+  
+  searchable do
+   text :title, :description
   end
 
   def to_s
