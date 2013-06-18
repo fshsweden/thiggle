5389000c106304f9f4046565ec9a77eb701520ed
Matt Rickard <mrckrd@gmail.com>
2013-06-18 18:50:59 +0000

    user account stuff

--- app/models/posting.rb
+++
@@ -1,9 +0,0 @@
-class Posting < ActiveRecord::Base
-  attr_accessible :category, :description, :location, :date, :post_id, :price, :title, :user_id
-  validates :title, :description, :post_id, :user_id, :category, :date, :presence => true
-  validates :user_id, :post_id, :numericality => {:greater_than_or_equal_to => 1}
-  validates :post_id, :uniqueness => true
-
-
-end
-
--- db/migrate/20130615014014_remove_field_name_from_postings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFieldNameFromPostings < ActiveRecord::Migration
-  def up
-    remove_column :postings, :post_id
-  end
-
-  def down
-    add_column :postings, :post_id, :integer
-  end
-end
