1a2c7d7b9336ac3fee58d0fdb2e37ac0c0d30f34
Matt Rickard <mrckrd@gmail.com>
2013-06-30 21:58:43 -0500

    more css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,18 @@
 class Post < ActiveRecord::Base
   belongs_to :user  
   has_one :user
+  has_attached_file :photo, {
+            :styles => { :large => "700x400#", :medium=>"490x368#", :thumbnail=>"75x75#" }}
+  
 
   searchable do
     text :title, :description
   end
 
-  attr_accessible :category, :description, :location, :price, :title, :user_id, :created_at
+  attr_accessible :category, :description, :location, :price, :title, :user_id, :created_at, :photo
   validates :title, :description, :user_id, :category, :presence => true
   validates :user_id, :numericality => {:greater_than_or_equal_to => 1}
+  validates_attachment_content_type :photo, :content_type => /image/
 
 end
 
