5e0616fe90f08b782aab169326a1d0d47afe7265
Matt Rickard <mrckrd@gmail.com>
2013-07-11 17:34:46 +0000

    added categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,6 +17,7 @@ class Post < ActiveRecord::Base
   attr_accessible :category, :description, :location, :price, :title, :user_id, :created_at, :photo, :photo2, :photo3, :photo4, :photo5
   validates :title, :description, :user_id, :category, :presence => true
   validates :user_id, :numericality => {:greater_than_or_equal_to => 1}
+  validates_length_of :description, :minimum => 5, :maximum => 160
   
 
 end
